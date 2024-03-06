c6f7b343435a2e8e96b386dde00247e1a441dd49
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-06 19:50:12 +0500

    correct the bool rightEmpty checking condition

--- 01_array_and_string/04_605_Can_Place_Flowers.cpp
+++ 01_array_and_string/04_605_Can_Place_Flowers.cpp
@@ -29,22 +29,22 @@ public:
   {
     int count = 0;
 
-    for(int i=0; i<flowerbed.size(); i++)
-        {
-            if(flowerbed[i] == 0)
-            {
-                bool leftEmpty = (i == 0) || (flowerbed[i-1] == 0);
-                bool rightEmpty = (i == flowerbed.size() -1) || (flowerbed[i+1] == 0);
+    for (int i = 0; i < flowerbed.size(); i++)
+    {
+      if (flowerbed[i] == 0)
+      {
+        bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+        bool rightEmpty = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
 
-                if(leftEmpty && rightEmpty)
-                {
-                    flowerbed[i] = 1;
-                    count++;
-                    if(count >= n)
-                        return true;
-                }
-            }
+        if (leftEmpty && rightEmpty)
+        {
+          flowerbed[i] = 1;
+          count++;
+          if (count >= n)
+            return true;
         }
+      }
+    }
 
     return count >= n;
   }
