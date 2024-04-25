d6d925856eeda611328beaa75df648b60cae92e1
Ghazanfar Ali <gak_92@hotmail.com>
2024-04-26 00:12:26 +0500

    60 unique paths

--- 19_dp_multidimensional/62. Unique Paths.cpp
+++ 19_dp_multidimensional/62. Unique Paths.cpp
@@ -2,10 +2,40 @@
 https://leetcode.com/problems/unique-paths/description/?envType=study-plan-v2&envId=leetcode-75
 */
 
-#include<iostream>
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+// Recursive solution with Memoization
+/*
+class Solution {
+private:
+    int dp[101][101];
+
+    int solve(int i, int j, int m, int n) {
+        if(i == m-1 && j == n-1)
+            return 1;
+
+        if(i<0 || i>m || j<0 || j>n)
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int right = solve(i, j+1, m, n);
+        int down = solve(i+1, j, m, n);
+
+        return dp[i][j] = right + down;
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 0, m, n);
+    }
+};
+*/
 
 class Solution
 {
@@ -28,7 +58,8 @@ public:
 
 int main()
 {
-
+  Solution s;
+  cout << s.uniquePaths(3, 7) << endl;
 
   return 0;
 }
