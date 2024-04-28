87cbc6eb376cb5aeeee75779ecb62a7f718f204f
Ghazanfar Ali <gak_92@hotmail.com>
2024-04-28 20:01:25 +0500

    338 Counting Bits

---
+++ 02_bit_manipulation/338. Counting Bits.cpp
@@ -0,0 +1,35 @@
+/*
+https://leetcode.com/problems/counting-bits/description/?envType=study-plan-v2&envId=leetcode-75
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> countBits(int n)
+  {
+    vector<int> res(n + 1);
+    res[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (i % 2 == 0)
+        res[i] = res[i / 2];
+      else
+        res[i] = res[i / 2] + 1;
+    }
+
+    return res;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> res = s.countBits(5);
+  for (int i = 0; i < res.size(); i++)
+    cout << res[i] << " ";
+  return 0;
+}
