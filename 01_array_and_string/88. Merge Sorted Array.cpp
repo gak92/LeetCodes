052ec2b7506e41c5b37d036a7d87f9f320281753
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-03 14:06:46 +0500

    88 Merge sorted array

---
+++ 01_array_and_string/88. Merge Sorted Array.cpp
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(j >= 0 && i >= 0) {
+            if(nums2[j] >= nums1[i]) {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+        }
+
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    s.merge(nums1, 3, nums2, 3);
+    for(int i = 0; i < nums1.size(); i++) {
+        cout << nums1[i] << " ";
+    }
+ return 0;
+}
