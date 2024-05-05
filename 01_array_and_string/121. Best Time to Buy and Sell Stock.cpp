b13addd78988965095256da98c4d405e0b04660b
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-05 15:02:48 +0500

    121 Best time to buy and sell stock

--- 01_array_and_string/121. Best Time to Buy and Sell Stock.cpp
+++ 01_array_and_string/121. Best Time to Buy and Sell Stock.cpp
@@ -5,6 +5,32 @@ https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+
+        while(r < prices.size()) {
+            if(prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = max(maxP, profit);
+            }
+            else {
+                l = r;
+            }
+            r++;
+        }
+
+        return maxP;
+    }
+};
+
+*/
+
 class Solution
 {
 public:
