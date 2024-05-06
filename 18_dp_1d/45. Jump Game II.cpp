2dc10a13eebc0146b538bfade184cf1e4e281aff
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-07 02:31:55 +0500

    45 Jump Game II

--- 18_dp_1d/45. Jump Game II.cpp
+++ 18_dp_1d/45. Jump Game II.cpp
@@ -60,6 +60,32 @@ public:
 };
 
 */
+
+// Greedy Solution - O(n)
+
+// Greedy optimal solution
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+
+        while(r < n-1) {
+            int farthest = 0;
+            for(int i=l; i<=r; i++) {
+                farthest = max(farthest, i+nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            jumps++;
+        }
+
+        return jumps;
+    }
+};
+
 int main()
 {
   vector<int> nums = {2, 3, 1, 1, 4};
