cc80896f3b73df3cd04f6886d8fa4f1ba04f5f8c
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-08 03:01:37 +0500

    238 product of array except self

--- 01_array_and_string/238. Product of Array Except Self.cpp
+++ 01_array_and_string/238. Product of Array Except Self.cpp
@@ -35,6 +35,66 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n);
+        vector<int> postfix(n);
+
+        prefix[0] = 1;
+        postfix[n-1] = 1;
+
+        for(int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            postfix[i] = postfix[i+1] * nums[i+1];
+        }
+
+        vector<int> res(n);
+        for(int i=0; i<n; i++) {
+            res[i] = prefix[i] * postfix[i];
+        }
+
+        return res;
+    }
+};
+
+*/
+
+/*
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+
+        res[0] = 1;
+        for(int i=1; i<n; i++) {
+            res[i] = res[i-1] * nums[i-1];
+        }
+
+        int rightProduct = 1;
+        for(int i=n-1; i>=0; i--) {
+            res[i] = res[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return res;
+    }
+};
+
+*/
+
 class Solution
 {
 public:
