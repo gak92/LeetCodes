ad2fe86ec8adf4fdc050c74227b561fdaf173ac0
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-09 20:37:38 +0500

    42 Trapping rain water using two pointer approach

--- 01_array_and_string/42. Trapping Rain Water.cpp
+++ 01_array_and_string/42. Trapping Rain Water.cpp
@@ -55,6 +55,37 @@ public:
   }
 };
 
+// Solution # 02 - Two pointer approach
+/*
+
+class Solution {
+
+public:
+    int trap(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        int maxLeft = height[i];
+        int maxRight = height[j];
+        int res = 0;
+
+        while(i < j) {
+            if(maxLeft <= maxRight) {
+                i++;
+                maxLeft = max(maxLeft, height[i]);
+                res += maxLeft - height[i];
+            }
+            else {
+                j--;
+                maxRight = max(maxRight, height[j]);
+                res += maxRight - height[j];
+            }
+        }
+
+        return res;
+    }
+};
+
+*/
 int main()
 {
   Solution s;
