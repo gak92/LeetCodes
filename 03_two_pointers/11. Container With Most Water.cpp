98a5c1fa3825f3fec620acf62df2abb003e5a69a
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-11 14:17:13 +0500

    11 Container with most water

--- 03_two_pointers/11. Container With Most Water.cpp
+++ 03_two_pointers/11. Container With Most Water.cpp
@@ -6,6 +6,29 @@ https://leetcode.com/problems/container-with-most-water/description/?envType=stu
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res = 0;
+        int l = 0;
+        int r = height.size() - 1;
+
+        while(l < r)
+        {
+            int area = (r - l) * min(height[l], height[r]);
+            res = max(res, area);
+
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return res;
+    }
+};
+*/
 class Solution
 {
 public:
