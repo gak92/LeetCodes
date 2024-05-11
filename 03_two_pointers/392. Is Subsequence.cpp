4092d5868b9cabd88d7e439d0c7270abacdc37f9
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-11 13:19:04 +0500

    392 Is subsequence

--- 03_two_pointers/392. Is Subsequence.cpp
+++ 03_two_pointers/392. Is Subsequence.cpp
@@ -7,6 +7,24 @@ https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+
+        while(i < s.length() && j < t.length()) {
+            if(s[i] == t[j])
+                i++;
+            j++;
+        }
+
+        return i == s.length();
+    }
+};
+*/
+
 class Solution
 {
 public:
