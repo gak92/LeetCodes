e20831df3d4f25d45d311c1b09b7f823b5208417
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-13 02:30:38 +0500

    48 Rotate Image - Optimized

--- 24_matrix/48. Rotate Image.cpp
+++ 24_matrix/48. Rotate Image.cpp
@@ -5,6 +5,45 @@ https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-inte
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int l = 0;
+        int r = matrix.size() - 1;
+
+        while(l < r)
+        {
+            for(int i=0; i<r-l; i++)
+            {
+                int top = l;
+                int bottom = r;
+
+                // save top left
+                int topLeft = matrix[top][l + i];
+
+                // move bottom left to top left
+                matrix[top][l + i] = matrix[bottom - i][l];
+
+                // move bottom right to bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i];
+
+                // move top right to bottom right
+                matrix[bottom][r - i] = matrix[top + i][r];
+
+                // move top left to top right
+                matrix[top + i][r] = topLeft;
+            }
+
+            l++;
+            r--;
+        }
+    }
+};
+
+*/
+
 class Solution
 {
 public:
