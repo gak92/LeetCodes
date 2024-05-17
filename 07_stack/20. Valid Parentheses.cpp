8ca40809b345594d8dbaf397d0c8f4f8012c998c
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-17 19:52:46 +0500

    20 Valid Parentheses

--- 07_stack/20. Valid Parentheses.cpp
+++ 07_stack/20. Valid Parentheses.cpp
@@ -6,6 +6,33 @@ https://leetcode.com/problems/valid-parentheses/description/
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char& ch:s)
+        {
+            if(ch == '(')
+                st.push(')');
+            else if(ch == '{')
+                st.push('}');
+            else if(ch == '[')
+                st.push(']');
+            else if(st.empty() || st.top() != ch)
+                return false;
+            else
+                st.pop();
+        }
+
+        return st.empty();
+    }
+};
+
+*/
+
 class Solution
 {
 public:
