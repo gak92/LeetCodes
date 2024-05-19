49d1f2d914563bf3a2c4b902aadf5f8bc8560736
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-19 13:17:40 +0500

    206 Reverse Linked List

--- 09_linked_list/206. Reverse Linked List.cpp
+++ 09_linked_list/206. Reverse Linked List.cpp
@@ -26,7 +26,29 @@ struct ListNode
 };
 
 /*
+// Recursive
 
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* last = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = NULL;
+
+        return last;
+    }
+};
+
+
+*/
+
+/*
+// Iterative
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
