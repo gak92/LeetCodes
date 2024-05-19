8f54b2bd06b3d135e9245352e2d47ea2311bd133
Ghazanfar Ali <gak_92@hotmail.com>
2024-05-19 13:50:02 +0500

    21 Merge two sorted lists

--- 09_linked_list/21. Merge Two Sorted Lists.cpp
+++ 09_linked_list/21. Merge Two Sorted Lists.cpp
@@ -25,6 +25,40 @@ struct ListNode
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+/*
+
+// Iterative Solution
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* res = new ListNode(-1);
+        ListNode* curr = res;
+
+        while(list1 != NULL && list2 != NULL) {
+            if(list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+
+        if(list1 == NULL)
+            curr->next = list2;
+        else
+            curr->next = list1;
+
+        return res->next;
+    }
+};
+
+
+*/
+
 class Solution
 {
 public:
