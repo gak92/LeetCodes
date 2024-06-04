045d0744d19014b728d44c2c5d79fa7690e98593
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-04 23:27:06 +0500

    409- Longest Palindrome (using map)

--- 01_array_and_string/409. Longest Palindrome.cpp
+++ 01_array_and_string/409. Longest Palindrome.cpp
@@ -5,6 +5,69 @@ https://leetcode.com/problems/longest-palindrome/description/
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution # 03 (using map and iterate only once)
+/*
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.length();
+        unordered_map<char, int> mp;
+        int oddFreq = 0;
+
+        for(auto& ch : s)
+        {
+            mp[ch]++;
+
+            if(mp[ch] % 2 == 0)
+                oddFreq--;
+            else
+                oddFreq++;
+        }
+
+        if(oddFreq > 0)
+            return n - oddFreq + 1;
+
+        return n;
+    }
+};
+
+*/
+
+// Solution # 02 (using map)
+/*
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.length();
+        unordered_map<char, int> mp;
+        int res = 0;
+        bool oddFreq = false;
+
+        for(auto& ch : s)
+            mp[ch]++;
+
+        for(auto& it : mp)
+        {
+            if(it.second % 2 == 0) {
+                res += it.second;
+            }
+            else {
+                res += it.second - 1;
+                oddFreq = true;
+            }
+        }
+
+        if(oddFreq)
+            res++;
+
+        return res;
+    }
+};
+
+*/
+
 // Solution # 01
 
 class Solution
