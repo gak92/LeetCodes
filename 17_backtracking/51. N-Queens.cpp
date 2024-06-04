4877baa199ff5dc95fec467b922213c6390adee9
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-05 02:31:23 +0500

    51- N-Queens (optimized approach)

--- 17_backtracking/51. N-Queens.cpp
+++ 17_backtracking/51. N-Queens.cpp
@@ -5,6 +5,61 @@ https://leetcode.com/problems/n-queens/description/
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution # 02 (optimized)
+/*
+
+class Solution {
+private:
+    int N;
+    vector<vector<string>> res;
+    unordered_set<int> columns;
+    unordered_set<int> diagonal;
+    unordered_set<int> antiDiag;
+
+    void solve(vector<string>& board, int row)
+    {
+        if(row >= N) {
+            res.push_back(board);
+            return;
+        }
+
+        for(int col=0; col<N; col++)
+        {
+            if(columns.find(col) != columns.end() ||
+                diagonal.find(row+col) != diagonal.end() ||
+                antiDiag.find(row-col) != antiDiag.end())
+                continue;
+
+            columns.insert(col);
+            diagonal.insert(row+col);
+            antiDiag.insert(row-col);
+            board[row][col] = 'Q';
+
+            solve(board, row + 1);
+
+            columns.erase(col);
+            diagonal.erase(row+col);
+            antiDiag.erase(row-col);
+            board[row][col] = '.';
+
+        }
+    }
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        N = n;
+        vector<string> board(n, string(n, '.'));
+
+        solve(board, 0);
+
+        return res;
+    }
+};
+
+*/
+
+// Solution # 01
+
 class Solution
 {
 private:
