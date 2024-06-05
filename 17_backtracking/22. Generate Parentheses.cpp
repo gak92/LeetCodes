a0ccf40d176a7e8c2ff35c77cea720935a8b3699
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-06 01:10:48 +0500

    22- Generate Parentheses (optimized)

--- 17_backtracking/22. Generate Parentheses.cpp
+++ 17_backtracking/22. Generate Parentheses.cpp
@@ -5,6 +5,45 @@ https://leetcode.com/problems/generate-parentheses/description/?envType=study-pl
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach 2: Backtracking with Optimisation
+/*
+class Solution {
+private:
+    vector<string> res;
+
+    void solve(string& curr, int n, int open, int close)
+    {
+        if(curr.length() == 2*n) {
+            res.push_back(curr);
+            return;
+        }
+
+        if(open < n) {
+            curr.push_back('(');
+            solve(curr, n, open+1, close);
+            curr.pop_back();
+        }
+
+        if(close < open) {
+            curr.push_back(')');
+            solve(curr, n, open, close+1);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        int open  = 0;
+        int close = 0;
+        solve(curr, n, open, close);
+
+        return res;
+    }
+};
+*/
+
+// Approach 1: Backtracking
+
 class Solution
 {
 private:
