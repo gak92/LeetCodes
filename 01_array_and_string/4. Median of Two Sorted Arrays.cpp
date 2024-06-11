a8d6d2ec2aa9dde0a11cc3f5dbf648ac5884edc0
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-11 12:52:33 +0500

    4- Median of two sorted array (using constant space)

--- 01_array_and_string/4. Median of Two Sorted Arrays.cpp
+++ 01_array_and_string/4. Median of Two Sorted Arrays.cpp
@@ -5,6 +5,75 @@ https://leetcode.com/problems/median-of-two-sorted-arrays/description/?envType=s
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution 02 - O(1) space
+/*
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int size = m + n;
+
+        int idx1 = size/2 - 1;
+        int element1 = -1;
+
+        int idx2 = size/2;
+        int element2 = -1;
+
+        while(i < m && j < n)
+        {
+            if(nums1[i] < nums2[j]) {
+                if(idx1 == k)
+                    element1 = nums1[i];
+                if(idx2 == k)
+                    element2 = nums1[i];
+                i++;
+            }
+            else {
+                if(idx1 == k)
+                    element1 = nums2[j];
+                if(idx2 == k)
+                    element2 = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < m) {
+            if(idx1 == k)
+                element1 = nums1[i];
+            if(idx2 == k)
+                element2 = nums1[i];
+            i++;
+            k++;
+        }
+
+        while(j < n) {
+            if(idx1 == k)
+                element1 = nums2[j];
+            if(idx2 == k)
+                element2 = nums2[j];
+            j++;
+            k++;
+        }
+
+        if(size%2 == 1) // odd
+            return element2;
+
+        return (element1 + element2) / 2.0;
+
+    }
+};
+
+*/
+
+// Solution 01 - O(n) space
+
 class Solution
 {
 public:
