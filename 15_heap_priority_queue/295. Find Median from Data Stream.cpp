c3d40851611dd3ebb8fef20699f96b9dff9212af
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-15 13:48:50 +0500

    295- Find Median from Data Stream

---
+++ 15_heap_priority_queue/295. Find Median from Data Stream.cpp
@@ -0,0 +1,74 @@
+/*
+https://leetcode.com/problems/find-median-from-data-stream/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class MedianFinder
+{
+private:
+  priority_queue<int> left_max_heap;
+  priority_queue<int, vector<int>, greater<int>> right_min_heap;
+
+public:
+  MedianFinder()
+  {
+  }
+
+  void addNum(int num)
+  {
+    if (left_max_heap.empty() || num < left_max_heap.top())
+    {
+      left_max_heap.push(num);
+    }
+    else
+    {
+      right_min_heap.push(num);
+    }
+
+    // now balance min and max heap
+    if (abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1)
+    {
+      right_min_heap.push(left_max_heap.top());
+      left_max_heap.pop();
+    }
+    else if (left_max_heap.size() < right_min_heap.size())
+    {
+      left_max_heap.push(right_min_heap.top());
+      right_min_heap.pop();
+    }
+  }
+
+  double findMedian()
+  {
+    double res;
+
+    if (left_max_heap.size() == right_min_heap.size())
+    {
+      res = (left_max_heap.top() + right_min_heap.top()) / 2.0;
+      return res;
+    }
+
+    return left_max_heap.top();
+  }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
+int main()
+{
+  MedianFinder *obj = new MedianFinder();
+  obj->addNum(1);
+  obj->addNum(2);
+  cout << obj->findMedian() << endl;
+  obj->addNum(3);
+  cout << obj->findMedian() << endl;
+
+  return 0;
+}
