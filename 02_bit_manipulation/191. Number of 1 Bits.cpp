63682e35cc358d96aaa8ccc849babcba7514a582
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-21 15:24:32 +0500

    191- Number of 1 bits (by unsetting right most bit)

--- 02_bit_manipulation/191. Number of 1 Bits.cpp
+++ 02_bit_manipulation/191. Number of 1 Bits.cpp
@@ -5,6 +5,26 @@ https://leetcode.com/problems/number-of-1-bits/?envType=study-plan-v2&envId=top-
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution # 02 - By unsetting(0) the rightmost set bit(1)
+/*
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+
+        while(n > 0)
+        {
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+};
+
+*/
+
 // Solution # 01 - Right shift the number and check if it is 1 by &(anding) with 1
 class Solution
 {
