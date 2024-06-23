5e8d1fc6e66ef9950d6d3be0dcfcf950ae96c411
Ghazanfar Ali <gak_92@hotmail.com>
2024-06-23 19:23:41 +0500

    201- Bitwise AND of Numbers Range

--- 02_bit_manipulation/201. Bitwise AND of Numbers Range.cpp
+++ 02_bit_manipulation/201. Bitwise AND of Numbers Range.cpp
@@ -5,6 +5,47 @@ https://leetcode.com/problems/bitwise-and-of-numbers-range/description/?envType=
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution # 02
+/*
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+
+        while(right > left)
+        {
+            right = right & (right - 1);
+        }
+
+        return right;
+    }
+};
+*/
+/*
+    n & (n-1) use cases:
+
+    1- clear the right most bit
+    e.g: 5      1 0 1   (5)
+                1 0 0   (4)
+                ------   &
+                1 0 0   (unset the right most set bit)
+
+    2- calculate the number of set bits
+    e.g: 5      1 0 1   (5)
+                1 0 0   (4)
+                ------  &
+                1 0 0   (4)
+
+    again       1 0 0   (4)
+                0 1 1   (3)
+                ------   &
+                0 0 0   (0)
+
+    I did it 2 times, so number of set bits is 2.
+
+    3- Check power of 2
+        n > 0 && { n & (n-1) == 0}
+*/
+
 class Solution
 {
 public:
