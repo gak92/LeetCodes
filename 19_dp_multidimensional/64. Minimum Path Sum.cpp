123d26aed8355177ea2c40e8959a2ff3a71e7c1f
Ghazanfar Ali <gak_92@hotmail.com>
2024-07-24 13:57:53 +0500

    Update 64. Minimum Path Sum.cpp (Bottom Up - Tabulation)

--- 19_dp_multidimensional/64. Minimum Path Sum.cpp
+++ 19_dp_multidimensional/64. Minimum Path Sum.cpp
@@ -30,3 +30,36 @@ public:
         return solve(grid, 0, 0, m, n, dp);
     }
 };
+
+// Solution # 02 (Bottom Up Approach)
+
+/*
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+
+        dp[0][0] = grid[0][0];
+        for(int col=1; col<n; col++)
+            dp[0][col] = grid[0][col] + dp[0][col-1];
+
+        for(int row=1; row<m; row++)
+            dp[row][0] = grid[row][0] + dp[row-1][0];
+
+        for(int row=1; row<m; row++)
+        {
+            for(int col=1; col<n; col++)
+            {
+                dp[row][col] = grid[row][col] + min(dp[row-1][col], dp[row][col-1]);
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
+
+
+*/
