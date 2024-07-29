ffe27da261258cd43b92c981757c685b652a4fbb
Ghazanfar Ali <gak_92@hotmail.com>
2024-07-29 12:15:11 +0500

    Update 97. Interleaving String.cpp (using 2 and 3 variables)

--- 19_dp_multidimensional/97. Interleaving String.cpp
+++ 19_dp_multidimensional/97. Interleaving String.cpp
@@ -2,7 +2,11 @@
 https://leetcode.com/problems/interleaving-string/description/?envType=study-plan-v2&envId=top-interview-150
 */
 
-// Solution # 01 - (Recursion + Memoization)
+
+// ======================================================================================
+// // Solution # 01 - (Recursion + Memoization) (using 3 variables)
+// ======================================================================================
+
 
 class Solution {
 private:
@@ -38,5 +42,47 @@ public:
 
         memset(dp, -1, sizeof(dp));
         return solve(0, 0, 0, s1, s2, s3);
+    }
+};
+
+// ======================================================================================
+// // Solution # 01 - (Recursion + Memoization) (using 2 variables)
+// ======================================================================================
+
+
+class Solution {
+private:
+    int n, m, N;
+    int dp[101][101];
+    bool solve(int i, int j, string& s1, string& s2, string& s3)
+    {
+        if(i == n && j == m && i+j == N)
+            return true;
+
+        if(i+j >= N)
+            return false;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        bool res = false;
+        if(s1[i] == s3[i+j])
+            res = solve(i+1, j, s1, s2, s3);
+        if(res == true)
+            return dp[i][j] = res;
+
+        if(s2[j] == s3[i+j])
+            res = solve(i, j+1, s1, s2, s3);
+
+        return dp[i][j] = res;
+    }
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        n = s1.length();
+        m = s2.length();
+        N = s3.length();
+
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, s1, s2, s3);
     }
 };
