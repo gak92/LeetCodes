cc141ef74b8ff7c2ad0dc310b8f57110858c44d2
Ghazanfar Ali <gak_92@hotmail.com>
2024-08-02 20:36:32 +0500

    Update 2134. Minimum Swaps to Group All 1's Together II.cpp (approach # 02)

--- 04_sliding_window/2134. Minimum Swaps to Group All 1's Together II.cpp
+++ 04_sliding_window/2134. Minimum Swaps to Group All 1's Together II.cpp
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/description/?envType=daily-question&envId=2024-08-02
 */
 
+// Approach # 01 (using extra space)
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
@@ -23,6 +24,36 @@ public:
 
             if(j-i+1 > totalOnes) {
                 currOnes -= temp[i];
+                i++;
+            }
+
+            maxCount = max(maxCount, currOnes);
+            j++; 
+        }
+
+        return totalOnes - maxCount;
+    }
+};
+
+
+// Approach # 02 (not using any extra space)
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        int totalOnes = accumulate(nums.begin(), nums.end(), 0);
+        int i = 0;
+        int j = 0;
+        int currOnes = 0;
+        int maxCount = 0;
+
+        while(j < 2*n) {
+            if(nums[j%n] == 1)
+                currOnes++;
+
+            if(j-i+1 > totalOnes) {
+                currOnes -= nums[i%n];
                 i++;
             }
 
