81b41a7f53d856ebadb5f2ebabd79e00f8ddd5c4
Ghazanfar Ali <gak_92@hotmail.com>
2024-08-05 12:40:39 +0500

    Update 322. Coin Change.cpp (using Tabulation)

--- 18_dp_1d/322. Coin Change.cpp
+++ 18_dp_1d/322. Coin Change.cpp
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/coin-change/description/?envType=study-plan-v2&envId=top-interview-150
 */
 
+// Solution # 01 - Recursion + Memoization (Top Down)
 class Solution {
 private:
     int solve(vector<int>& coins, int amount, vector<int>& dp)
@@ -24,5 +25,26 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1, -2);
         return solve(coins, amount, dp);        
+    }
+};
+
+// Solution # 02 - Tabulation (Bottom Up)
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX-1);
+        dp[0] = 0;
+
+        for(int amnt=1; amnt<=amount; amnt++)
+        {
+            for(int coin : coins)
+            {
+                if(amnt - coin >= 0)
+                    dp[amnt] = min(dp[amnt], dp[amnt - coin] + 1);
+            }
+        }
+
+        return dp[amount] == INT_MAX-1 ? -1 : dp[amount]; 
     }
 };
